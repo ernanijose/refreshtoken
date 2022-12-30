e3b39511793fb2a0f204a400324cd8050805de13
ernanijose <ernanijose@gmail.com>
2022-12-29 22:34:29 -0300

    Tratando os erros de excessoes gerados

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
-import express from "express";
+import "express-async-errors";
+import express, {Request, Response, NextFunction } from "express";
 import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+//aqui é para trabalhar com as rotas em um arquivo
 app.use(router);
+//aqui é para tratar as excessoes de error
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+    return response.status(500).json({
+        status: "Error",
+        message: error.message
+    });
+});
 
 const PORT = 3535;
 app.listen(PORT, () =>{
